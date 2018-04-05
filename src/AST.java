5936c1a35f7d629f82cdc0674bb73bbd688a6932
Edgar <e_berber@u.pacific.edu>
2018-04-05 15:39:18 -0700

    almost fixed

--- src/AST.java
+++ src/AST.java
@@ -1,58 +1,113 @@
-
-public class AST {
-
-
-	// ASTs at most have 3 children (3 is used for if statements).
-	// For every other statement and expression, at least one child will be null.
-	Token token;
-	public Token getToken() {
-		return token;
-	}
-
-	public void setToken(Token token) {
-		this.token = token;
-	}
-
-	public AST getLeft() {
-		return left;
-	}
-
-	public void setLeft(AST left) {
-		this.left = left;
-	}
-
-	public AST getMiddle() {
-		return middle;
-	}
-
-	public void setMiddle(AST middle) {
-		this.middle = middle;
-	}
-
-	public AST getRight() {
-		return right;
-	}
-
-	public void setRight(AST right) {
-		this.right = right;
-	}
-
-	AST left;
-	AST middle;
-	AST right;
-	AST(Token t, AST l, AST m, AST r){
-		this.token = t;
-		this.left = l;
-		this.middle = m;
-		this.right = r;
-	}
-	/* Also define getter and setter methods*/
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-} class ParsingException extends Exception { 
-
-} // this is to generate Parsing errors
+//import java.util.ArrayList;
+//
+//class AST {// ASTs at most have 3 children (3 is used for if statements).
+//	// For every other statement and expression, at least one child will be null.
+//	Token token;AST left;AST middle;AST right;
+//	AST(Token t, AST l, AST m, AST r){
+//		this.token = t;
+//		this.left = l;this.middle = m;this.right = r;
+//	}
+//	//Also define getter and setter methods
+//	}
+//class ParsingException extends Exception { 
+//	
+//} 
+//// this is to generate Parsing errors
+//ArrayList<Token> scanner() {
+//	ArrayList<Token> list = new ArrayList<Token> ();
+//	//This is where you define your scanner.   It is supposed to return a list. 
+//			return list;
+//}
+//ArrayList<Token> tokenList = new ArrayList<Token> (); 
+////list of tokens accessible to all parsing methods
+//int index = 0; // index in the list of tokens
+//Token nextToken(){ // returns the token in the current index
+//	return tokenList.get(index);
+//}
+//void consumeToken(){ // shifts the token forward
+//	index++;
+//}
+//void printAST(AST tree){ //print the resulting AST to output file 
+//
+//}AST parseStatement() throws ParsingException {AST tree = parseBaseStatement();
+//while (nextToken().value == ";")
+//{Token t = nextToken();consumeToken();tree = new AST (t, tree, parseBaseStatement(), null);}
+//return tree;}
+//AST parseBaseStatement() throws  ParsingException {AST tree;
+//if (nextToken().type == "Identifier") {tree = parseAssignment();
+//}
+//else if (nextToken().value == "if") {
+//	tree = parseIfStatement();
+//}
+//else if (nextToken().value == "while"){
+//	tree = parseWhileStatement();
+//}
+//else if (nextToken().value == "skip") {
+//	tree = parseSkip();}
+//else {throw new ParsingException();
+//}
+//return tree;
+//}
+//AST parseAssignment() throws ParsingException {
+//	/* check whether nextToken is an identifier 
+//	 *   if so, make a AST tree1 with token consisting of the identifier, 
+//	 *    and null children.     
+//	 *     next check if the next token is assignment operator.
+//	 *     if so make a token t2.    
+//	 *     next return a tree with t2 in the root, left child to be tree1,and right child to be the result of parsing expression.
+//	 *     otherwise generate parsing error   
+//	 *     otherwise generate parsing Error 
+//	 *     */
+//}
+//AST parseIfStatement() throws ParsingException {
+//	//	 check whether nextToken is "if".   
+//	// * if so, make a token t1 for if statement, and consume the token.      
+//	// * next parse boolean expression and assign it to AST tree1.
+//	// * check if the next token is "then".
+//	// * if so, consume the token, parse the statement and assign the result to tree2.
+//	// * check if the next token is "else".
+//	// * if so, consume the token, parse the statement and assign the result to tree3.
+//	// * check if next token is "endif".
+//	// * if so, consume the token, and return a tree with
+//	// * token t1 at the root,tree1 as the left child
+//	// * tree2 as the middle child
+//	// * tree3 as the right child
+//	// * otherwise generate parsing error
+//	// * otherwise generate parsing error
+//	// * otherwise generate parsing error
+//	// * otherwise generate parsing error
+//}AST parseWhileStatement() throws ParsingException {
+//	//	 check whether next token is "while"   
+//	// * if so, make a token t1 for while loops, and consume the token.
+//	//		next parse the boolean expression and assign it to AST tree1.
+//	// if the next token is "do"if so, consume the token, parse the statement and assign the result to tree2.
+//	// check whether the next token is "endwhile"
+//	// if so, consume the token, and return a tree with
+//	// token t1 at the	 roottree1 as the left childtree2 as the middle child
+//	// null as the right child
+//	// otherwise generate parsing error
+//	// otherwise generate parsing error  
+//	// otherwise generate parsing error
+//}
+//AST parseSkip() throws ParsingException {
+//	//	 check whether the next token is "skip"   
+//	// * if so, make a token t1 containing "skip" and its type (KEYWORD).
+//	// * return a tree with
+//	// * token t1 as its root
+//	// * null for left, middle and right children 
+//	//	otherwise generate parsing error
+//}
+//// similarly define a method to parse each of the following:
+//// * expressions
+//// * numexpressions
+//// * numterms
+//// * numfactors
+//// * numpieces
+//// * numelements
+//// * numbersidentifiersboolexpressionsbooltermsboolfactorsboolpiecesboolelementsbooleans
+//
+//
+//
+//
+//
+//
--- src/Token.java
+++ src/Token.java
@@ -32,7 +32,7 @@ public class Token {
 			return new ArrayList<Token>();
 		}
 		else 
-		return list;
+			return list;
 	}
 
 	Token (String v, String t){
@@ -42,7 +42,7 @@ public class Token {
 
 	static String[] split(String str1) { //cuts white spaces
 		//everywhite space is a new element
-		String []str2= null;
+		String []str2;
 		str2 = new String[200];
 		Scanner scanner = new Scanner(str1).useDelimiter("\\s"); 
 		// Printing the delimiter used
@@ -63,61 +63,65 @@ public class Token {
 		for(int i=0;i <str1.length; i++){ //for each element of the array determine what its token is
 			if(str1[i] != null) {
 				System.out.println("Passing to token string: " + str1[i]);
-				//tokenizeSepSpace(str1[i]);
+				tokenizeSepSpace(str1[i]);
 			}
 			else{ 
-				 throw new IllegalArgumentException("The argument cannot be null in sepSpaces");
+				return;
+				//throw new IllegalArgumentException("The argument cannot be null in sepSpaces");
 			}
 		}
 
 	}
 
-
-
-
-
 	public static void tokenizeSepSpace(String string) { //determines what the array elements token is and value to add to Token list
 		// TODO Auto-generated method stub
-		Token t =null;
+		Token t1 =null;
+		Token t2 =null;
 		String s;
 		for(int i=0; i < string.length(); i++) {
+			t1 = null;
 			System.out.println("string passed in:" + string);
 			s = string.substring(0, i+1);
-			System.out.println("substring:" + string.substring(0, i));
+			System.out.println("substring:" + string.substring(0,i+1));
 			System.out.println("string s:" + s);
 
 			if(isIdentify(s)) { 
 				System.out.println("Match at identify");
-				t= new Token(s, "Identify");
+				t1= new Token(s, "Identify");
 			}
 			else if(isNumber(s)) {
 				System.out.println("Match at Number");
-				t= new Token(s, "Number");
-			}
-			else if(isKeyword(s)) {
-				System.out.println("Match at keyword");
-				t= new Token(s, "Keyword");
+				t1= new Token(s, "Number");
 			}
 			else if(isBoolean(s)) {
 				System.out.println("Match at bool");
-				t= new Token(s, "Bool");
+				t1= new Token(s, "Bool");
 			}
 			else if(isPunctuation(s)) {
 				System.out.println("Match at Punc");
-				t= new Token(s, "Punctuation");
+				t1= new Token(s, "Punctuation");
 			}
-			
-				if(t!= null) {
-					System.out.println("Made an addition to list");
-					list.add(t);
-					tokenizeSepSpace(string.substring(i, string.length()-1));
-					break;
-				}
-				else if(t==null) {
-					 throw new IllegalArgumentException("The argument cannot be null");
-				}
-			
+			else if(isKeyword(s)) {
+				System.out.println("Match at keyword");
+				t1= new Token(s, "Keyword");
+			}
+				if(t1!= null) {
+					t2=new Token(t1.getValue(),t1.getToken());
+					System.out.println("here" + t2.value);
 
+				}
+				if(t1==null) {
+					if(t2==null) {
+						System.out.println("Error");
+						return;
+					}
+					else {
+						System.out.println("Made an addition to list" + t2.value );
+						list.add(t2);
+						tokenizeSepSpace(string.substring(i, string.length()-1));
+						break;
+					}
+				}
 		}
 	}
 
@@ -191,10 +195,10 @@ public class Token {
 	}
 
 	private static void printArraylist(){
-		//for(int i =0; i< list.size(); i++) {
-		//System.out.println("List value: " + Arrays.toString(list.toArray()));
-		System.out.println("List token: " + list.get(1).getToken());
-		//}
+		for(int i =0; i< list.size(); i++) {
+			System.out.println("List value: " + list.get(i).getValue());
+			System.out.println("List token: " + list.get(i).getToken());
+		}
 	}
 
 	public static void main(String[] args) throws IOException {
@@ -208,121 +212,9 @@ public class Token {
 
 		// Printing the read line
 		System.out.println(tokenizeMe);  
-			sepSpaces(split(tokenizeMe));
+		sepSpaces(split(tokenizeMe));
 		printArraylist();
 	}
 }
 
 
-
-/*         START OF PARSER                    */
-
-/*
-class AST {// ASTs at most have 3 children (3 is used for if statements).
-	// For every other statement and expression, at least one child will be null.
-	Token token;AST left;AST middle;AST right;
-	AST(Token t, AST l, AST m, AST r){
-		this.token = t;
-		this.left = l;this.middle = m;this.right = r;
-	}
- Also define getter and setter methods}
-class ParsingException extends Exception { } 
-// this is to generate Parsing errors
-ArrayList<Token> scanner() {
-	ArrayList<Token> list = new ArrayList<Token> ();
-	 This is where you define your scanner.   It is supposed to return a list. 
-	return list;
-}
-ArrayList<Token> tokenList = new ArrayList<Token> (); 
-//list of tokens accessible to all parsing methods
-int index = 0; // index in the list of tokens
-Token nextToken(){ // returns the token in the current index
-	return tokenList.get(index);
-}
-void consumeToken(){ // shifts the token forward
-	index++;
-}
-void printAST(AST tree){ print the resulting AST to output file 
-
-}AST parseStatement() throws ParsingException {AST tree = parseBaseStatement();
-while (nextToken().value == ";")
-{Token t = nextToken();consumeToken();tree = new AST (t, tree, parseBaseStatement(), null);}
-return tree;}
-AST parseBaseStatement() throws  ParsingException {AST tree;
-if (nextToken().type == "Identifier") {tree = parseAssignment();
-}
-else if (nextToken().value == "if") {
-	tree = parseIfStatement();
-}
-else if (nextToken().value == "while"){
-	tree = parseWhileStatement();
-}
-else if (nextToken().value == "skip") {
-	tree = parseSkip();}
-else {throw new ParsingException();
-}
-return tree;
-}
-AST parseAssignment() throws ParsingException {
-	 check whether nextToken is an identifier 
-	 *   if so, make a AST tree1 with token consisting of the identifier, 
-	 *    and null children.     
-	 *     next check if the next token is assignment operator.
-	 *     if so make a token t2.    
-	 *     next return a tree with t2 in the root, left child to be tree1,and right child to be the result of parsing expression.
-	 *     otherwise generate parsing error   
-	 *     otherwise generate parsing Error 
-}
-AST parseIfStatement() throws ParsingException {
-	 check whether nextToken is "if".   
-	 * if so, make a token t1 for if statement, and consume the token.      
-	 * next parse boolean expression and assign it to AST tree1.
-	 * check if the next token is "then".
-	 * if so, consume the token, parse the statement and assign the result to tree2.
-	 * check if the next token is "else".
-	 * if so, consume the token, parse the statement and assign the result to tree3.
-	 * check if next token is "endif".
-	 * if so, consume the token, and return a tree with
-	 * token t1 at the root,tree1 as the left child
-	 * tree2 as the middle child
-	 * tree3 as the right child
-	 * otherwise generate parsing error
-	 * otherwise generate parsing error
-	 * otherwise generate parsing error
-	 * otherwise generate parsing error
-}AST parseWhileStatement() throws ParsingException {
-	 check whether next token is "while"   
-	 * if so, make a token t1 for while loops, and consume the token.
-		next parse the boolean expression and assign it to AST tree1.
- if the next token is "do"if so, consume the token, parse the statement and assign the result to tree2.
- check whether the next token is "endwhile"
- if so, consume the token, and return a tree with
- token t1 at the	 roottree1 as the left childtree2 as the middle child
- null as the right child
- otherwise generate parsing error
- otherwise generate parsing error  
- otherwise generate parsing error
-}
-AST parseSkip() throws ParsingException {
-	 check whether the next token is "skip"   
-	 * if so, make a token t1 containing "skip" and its type (KEYWORD).
-	 * return a tree with
-	 * token t1 as its root
-	 * null for left, middle and right children 
-	otherwise generate parsing error
-}
- similarly define a method to parse each of the following:
- * expressions
- * numexpressions
- * numterms
- * numfactors
- * numpieces
- * numelements
- * numbersidentifiersboolexpressionsbooltermsboolfactorsboolpiecesboolelementsbooleans
-
-
-
-
-
-
-*/
